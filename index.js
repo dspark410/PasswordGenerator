d35d27977aa70d50c1bd27b0660fd64fd46cad2b
mistahpig <dspark410@gmail.com>
2020-04-22 16:56:58 -0700

    adds while loops to js file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-var password = prompt(" pick a password");
+var specialCharacters = confirm("Would you like to use special characters");
+var numericChracters = confirm("Would you like to use numeric characters");
+var lowercaseCharacters = confirm("Would you like to use lowercase characters");
+var uppercaseCharacters = confirm("Would you like to use uppercase characters");
+
+function generatePassword(){
+    var passwordQuestion = 0;
+    while (
+        passwordQuestion < 8 ||
+        passwordQuestion > 128 ||
+        isNaN(passwordQuestion)
+    ) {
+        var passwordQuestion = prompt("Choose the length of the password you would like between 8 and 128 characters");
+    }
+};
+
+
+
+
+for (let i = 0; i < .length; i++) {
+    var element = array[i];
+
+}

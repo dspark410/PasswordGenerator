c28e9ea39e1f10ffefdec200af38239103ad227e
mistahpig <dspark410@gmail.com>
2020-04-25 16:39:28 -0700

    finishes homework with alerts

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var specialCharacters = confirm("Would you like to use special characters");
-var numericChracters = confirm("Would you like to use numeric characters");
-var lowercaseCharacters = confirm("Would you like to use lowercase characters");
-var uppercaseCharacters = confirm("Would you like to use uppercase characters");
-
-function generatePassword(){
+function generatePassword() {
     var passwordQuestion = 0;
     while (
         passwordQuestion < 8 ||
@@ -12,12 +7,51 @@ function generatePassword(){
     ) {
         var passwordQuestion = prompt("Choose the length of the password you would like between 8 and 128 characters");
     }
-};
+        
+    var specialCharacters = confirm("Would you like to use special characters?");
+    var numericCharacters = confirm("Would you like to use numeric characters?");
+    var lowercaseCharacters = confirm("Would you like to use lowercase characters?");
+    var uppercaseCharacters = confirm("Would you like to use uppercase characters?");
+    
+    while (
+        specialCharacters === false && 
+        numericCharacters === false&& 
+        lowercaseCharacters === false && 
+        uppercaseCharacters === false
+        ) {
+        alert("Please choose at least 1 character criteria.")
+        var specialCharacters = confirm("Would you like to use special characters?");
+        var numericCharacters = confirm("Would you like to use numeric characters?");
+        var lowercaseCharacters = confirm("Would you like to use lowercase characters?");
+        var uppercaseCharacters = confirm("Would you like to use uppercase characters?");
+    }
+    
+        var special = "!”#$%&(*+,-./:;<=>?@[^_`{|}]";
+        var nums = "0123456789";
+        var lower = "abcdefghijklmnopqrstuvwxyz";
+        var upper = "ABCDEFGHIJKLMNOPARSTUVQXYZ";
+        var allowed = "";
+        
+        if (specialCharacters === true) {
+            allowed += special
+        }
+        if (numericCharacters === true) {
+            allowed += nums
+        }
+        if (lowercaseCharacters === true) {
+            allowed += lower
+        }
+        if (uppercaseCharacters === true) {
+            allowed += upper
+        }
 
+        var totalPassword = "";
+        for (var i = 0; i < passwordQuestion; i++){
+            var random = Math.floor(Math.random() * allowed.length)
+            totalPassword += allowed[random];
+        }
 
-
-
-for (let i = 0; i < .length; i++) {
-    var element = array[i];
-
+    alert(totalPassword);
 }
+
+generatePassword()
